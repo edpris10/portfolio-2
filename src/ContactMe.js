becf442916f8763b24e47efc00b32e84946c9e03
Priscilla <edpris.montalvo@gmail.com>
2022-12-22 00:19:17 -0600

    address

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -8,7 +8,7 @@ function ContactMe() {
             Contact Info
         </p>    
         <address>
-            18200 Blanco Springs <br />
+            24345 Wilderness Oak <br />
             San Antonio TX 78258 <br />
             PH: (830)765-5453 <br />
             Email: <a href="mailto:prisc.montalvo@gmail.com">Prisc.montalvo@gmail.com</a>
